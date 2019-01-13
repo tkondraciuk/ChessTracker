67bbba734d7fd77ab874a7b659498e3d97802ba1
Unknown <tomek.kondraciuk@op.pl>
2019-01-13 17:59:34 +0100

    Przeniesienie metody findMarkers() do klasy ChessboardField

--- Chessboardfield.py
+++ Chessboardfield.py
@@ -3,12 +3,25 @@ import cv2
 
 class ChessboardField:
 
+    marker_min=np.array([43,54,27], dtype=np.uint8)
+    marker_max=np.array([75,167,131], dtype=np.uint8)
+
+    whitePiece_min=[]
+    whitePiece_max=[]
+
+    blackPiece_min=np.array([0,0,0], dtype=np.uint8)
+    blackPiece_max=np.array([2,239,255], dtype=np.uint8)
 
     def __init__(self, label, image):
+        self.marker=self.findMarkers(image)
         self.image=image
         self.label=label
 
     def hasChanged(self):
         quantile=np.quantile(self.image,0.5)
         return quantile>=128
-    
+
+    def findMarkers(self,image):
+        hsv=cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
+        mask=cv2.inRange(hsv,self.marker_min, self.marker_max)
+        return mask
--- FieldSeparator.py
+++ FieldSeparator.py
@@ -5,12 +5,7 @@ import ChessboardField
 import ChessboardField as CF
 from math import *
 
-def findMarkers(image):
-        hsv=cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
-        cmin=np.array([43,54,27], dtype=np.uint8)
-        cmax=np.array([75,167,131], dtype=np.uint8)
-        mask=cv2.inRange(hsv,cmin, cmax)
-        return mask
+
 
 class FieldSeparator:
     fields=[]
@@ -18,7 +13,6 @@ class FieldSeparator:
     def __init__(self,calibration):
         self.fieldVerticles=calibration.verticles
         self.image=calibration.cameraHandler.GetFrame()
-        self.image=findMarkers(self.image)
 
     def getFieldsImage(self):
         def cutImage(self,image,p1,p2):
