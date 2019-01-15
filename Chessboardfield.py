51fd2fb0e897f2dae69bb2a923e757f256385078
Unknown <tomek.kondraciuk@op.pl>
2019-01-15 20:18:55 +0100

    Dodanie mechanizmu sprawdzającego stan pola
    Mechanizm sprawdza czy pole jest puste, a jeżeli nie to jakiego koloru jest stojąca na nim figura

--- Chessboardfield.py
+++ Chessboardfield.py
@@ -1,10 +1,19 @@
 import numpy as np
 import cv2
 
+FIELD_STATE_UNKNOWN=-1
+FIELD_EMPTY=0
+FIELD_WHITE_PIECE=1
+FIELD_BLACK_PIECE=2
+
+def imopen(image, kernelSize):
+    kernel=np.ones(kernelSize,np.uint8)
+    return cv2.morphologyEx(image,cv2.MORPH_OPEN,kernel)
+
 class ChessboardField:
 
-    marker_min=np.array([43,54,27], dtype=np.uint8)
-    marker_max=np.array([75,167,131], dtype=np.uint8)
+    marker_min=np.array([38,33,0], dtype=np.uint8)
+    marker_max=np.array([83,255,255], dtype=np.uint8)
 
     whitePiece_min=[]
     whitePiece_max=[]
@@ -13,9 +22,10 @@ class ChessboardField:
     blackPiece_max=np.array([2,239,255], dtype=np.uint8)
 
     def __init__(self, label, image):
-        self.marker=self.findMarkers(image)
-        self.image=image
+        self.updateImage(image, checkState=False)
+        self.emptyFieldMarker=self.marker
         self.label=label
+        self.state=FIELD_STATE_UNKNOWN
 
     def hasChanged(self):
         quantile=np.quantile(self.image,0.5)
@@ -26,9 +36,37 @@ class ChessboardField:
         mask=cv2.inRange(hsv,self.marker_min, self.marker_max)
         return mask
 
-    def findBlackPiece(self,image):
-        hsv=cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
-        mask=cv2.inRange(hsv,self.blackPiece_min, self.blackPiece_max)
+    def checkFieldState(self):
+        def getNonZeroValues(a):
+            l=[]
+            for row in a:
+                for f in row:
+                    if f!=0:
+                        l.append(f)
+            return l
+        if self.isEmpty():
+            self.state=FIELD_EMPTY
+            return
+        pieceMask=cv2.bitwise_xor(self.markerMask,self.emptyFieldMarker)
+        pieceColorSample=cv2.bitwise_and(self.image,self.image,mask=pieceMask)
+        pieceColorSample=cv2.cvtColor(pieceColorSample, cv2.COLOR_BGR2GRAY)
+
+        if np.median(getNonZeroValues(pieceColorSample))>30:
+            self.state=FIELD_WHITE_PIECE
+        else:
+            self.state=FIELD_BLACK_PIECE
+
+    def isEmpty(self):
+        return np.any(self.marker)
+        
+
+
+    def updateImage(self, image, checkState=True):
+        self.image=image
+        self.markerMask=self.findMarkers(image)
+        self.marker=imopen(self.markerMask,(8,8))
+        if checkState:
+            self.checkFieldState()
 
     
     
--- FieldSeparator.py
+++ FieldSeparator.py
@@ -12,18 +12,29 @@ class FieldSeparator:
 
     def __init__(self,calibration):
         self.fieldVerticles=calibration.verticles
-        self.image=calibration.cameraHandler.GetFrame()
+        self.cameraHandler=calibration.cameraHandler
+        self.image=self.cameraHandler.GetFrame()
 
-    def getFieldsImage(self):
-        def cutImage(self,image,p1,p2):
-            return image[p1[1]:p2[1],p1[0]:p2[0]]
-        self.fields=[]
-        
+    
+    def cutImage(self,p1,p2):
+        return self.image[p1[1]:p2[1],p1[0]:p2[0]]
+    def createChessboardFields(self):
+        for i in range(8):
+            row=[]
+            for j in range(8):
+                p1=self.fieldVerticles[i][j]
+                p2=self.fieldVerticles[i+1][j+1]
+                fieldImage=self.cutImage(p1,p2)
+                field=CF.ChessboardField('',fieldImage)
+                row.append(field)
+            self.fields.append(row)
+    def updateChessboardFields(self):
+        self.image=self.cameraHandler.GetFrame()
         for i in range(8):
             for j in range(8):
                 p1=self.fieldVerticles[i][j]
                 p2=self.fieldVerticles[i+1][j+1]
-                fieldImage=cutImage(self,self.image,p1,p2)
-                field=CF.ChessboardField('',fieldImage)
-                self.fields.append(field)
+                fieldImage=self.cutImage(p1,p2)
+                self.fields[i][j].updateImage(fieldImage,False) # Usunąć false
+
 
