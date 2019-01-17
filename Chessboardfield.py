6439bf9693abb41f3aa78e654a880282fab14cc1
Unknown <tomek.kondraciuk@op.pl>
2019-01-17 14:14:28 +0100

    Udoskonalenie mechanizmu sprawdzającego stan pola
    Teraz mechanizm popełnia mniej błędów

--- Chessboardfield.py
+++ Chessboardfield.py
@@ -12,14 +12,14 @@ def imopen(image, kernelSize):
 
 class ChessboardField:
 
-    marker_min=np.array([38,33,0], dtype=np.uint8)
+    # Markery na dzien
+    # marker_min=np.array([38,33,0], dtype=np.uint8)
+    # marker_max=np.array([83,255,255], dtype=np.uint8)
+
+    # Markery na noc
+    marker_min=np.array([31,33,0], dtype=np.uint8)
     marker_max=np.array([83,255,255], dtype=np.uint8)
 
-    whitePiece_min=[]
-    whitePiece_max=[]
-
-    blackPiece_min=np.array([0,0,0], dtype=np.uint8)
-    blackPiece_max=np.array([2,239,255], dtype=np.uint8)
 
     def __init__(self, label, image):
         self.updateImage(image, checkState=False)
@@ -27,9 +27,6 @@ class ChessboardField:
         self.label=label
         self.state=FIELD_STATE_UNKNOWN
 
-    def hasChanged(self):
-        quantile=np.quantile(self.image,0.5)
-        return quantile>=128
 
     def findMarkers(self,image):
         hsv=cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
@@ -51,7 +48,7 @@ class ChessboardField:
         pieceColorSample=cv2.bitwise_and(self.image,self.image,mask=pieceMask)
         pieceColorSample=cv2.cvtColor(pieceColorSample, cv2.COLOR_BGR2GRAY)
 
-        if np.median(getNonZeroValues(pieceColorSample))>30:
+        if np.median(getNonZeroValues(pieceColorSample))>45:
             self.state=FIELD_WHITE_PIECE
         else:
             self.state=FIELD_BLACK_PIECE
@@ -64,7 +61,7 @@ class ChessboardField:
     def updateImage(self, image, checkState=True):
         self.image=image
         self.markerMask=self.findMarkers(image)
-        self.marker=imopen(self.markerMask,(8,8))
+        self.marker=imopen(self.markerMask,(6,6))
         if checkState:
             self.checkFieldState()
 
