7736e6757f15ddae6552291bcca0d326ef827b0d
Unknown <tomek.kondraciuk@op.pl>
2019-04-09 13:54:25 +0200

    Dodano menu umożliwiające wybór kamery

--- Main.py
+++ Main.py
@@ -5,6 +5,34 @@ import FieldSeparator as FS
 import time
 import CameraHandler
 import keyboard as key
+from tkinter import *
+import device as dev
+
+root=Tk()    
+def onButtonClick():
+        root.destroy()
+
+def getCameraId(): 
+    cameras=dev.getDeviceList()
+    
+    label=Label(root, text='Wybierz kamere:')
+    label.pack()
+
+    currentChoice=StringVar(root, cameras[0])
+    DDmenu=OptionMenu(root, currentChoice, *cameras)
+    DDmenu.pack()
+
+    button=Button(root, text='Dalej', command=onButtonClick) 
+    button.pack()
+
+    mainloop()
+    
+    chosenCameraName=currentChoice.get()
+    return cameras.index(chosenCameraName)
+
+
+
+    
 
 
 def readCameraId():
@@ -16,13 +44,13 @@ def readCameraId():
     else:
         return 0
     
+cameraId= getCameraId() #readCameraId()
 
 print('Usun wszystkie figury z planszy i wcisnij Spacje')
 while True:
     if key.is_pressed('Space'):
         break
 
-cameraId=readCameraId()
 ch=CameraHandler.CameraHandler(cameraId) 
 calib=Calibration.Calibration(ch)
 calib.StartCalibration()
@@ -38,18 +66,6 @@ while True:
         i=0 
         message=chessboardState.Update()
         print(message)
-        for field in chessboardState.fields.values():
-            factor=None
-            if field.state==0:
-                factor=128
-            elif field.state==1:
-                factor=225
-            elif field.state==2:
-                factor=0
-            else:
-                raise Exception('Blad w okreslaniu stanu pola')
-                
-            cv2.imwrite('Pola/{}.jpg'.format(field.getName()),np.ones((30,30) ,dtype=np.uint8)*factor)
-
+        
         print("Wykonaj następny ruch i wcisnij spacje.")
            
